1e8a9f7724297ccfd259253be3b19277c3fe56e0
TobiasKonitzer <tobiask@stanford.edu>
2015-02-18 11:59:22 -0500

    Update fit.R

--- fit.R
+++ fit.R
@@ -8,8 +8,7 @@ irtfit<-function(model, data)
  
  if(class(model)=="SingleGroupClass"){
    holder<-matrix(NA, nrow=nrow(data),ncol=ncol(data))
-   colnames(holder)<-colnames(data)
-   fscores_holder<-fscores(model,full.scores=T, scores.only=T)
+   colnames(holder)<-colnames(data);fscores_holder<-fscores(model,full.scores=T, scores.only=T)
    for (i in 1:ncol(data)){
      holder[,i]<-plogis(coef(model)[[i]][2]+coef(model)[[i]][1]*fscores_holder)
    }
